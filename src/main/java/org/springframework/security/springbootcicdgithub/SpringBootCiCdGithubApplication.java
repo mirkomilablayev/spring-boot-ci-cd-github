aa40bb43f9dd47789d45804696ffce59e3878575
ablayev1999 <ablayev1999@gmail.com>
2025-11-06 19:55:14 +0500

    second commit

---
+++ src/main/java/org/springframework/security/springbootcicdgithub/SpringBootCiCdGithubApplication.java
@@ -0,0 +1,20 @@
+package org.springframework.security.springbootcicdgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootCiCdGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCiCdGithubApplication.class, args);
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "I'm Alive 😅";
+    }
+}
---
+++ src/test/java/org/springframework/security/springbootcicdgithub/SpringBootCiCdGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.security.springbootcicdgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCiCdGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
